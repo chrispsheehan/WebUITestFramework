d55cf080ea4cd98468b6b35d950bb99edc50ea20
chrispsheehan <chris_sheehan@hotmail.co.uk>
2020-10-16 14:05:14 +0100

    added settings.json file

--- src/Framework.Settings/TestSettings.cs
+++ src/Framework.Settings/TestSettings.cs
@@ -2,7 +2,6 @@ namespace Framework.Settings
 {
     public class TestSettings
     {
-        public string BaseUrl { get; set; }
         public double WaitSeconds { get; set; }
         public string BrowserName { get; set; }
         public string BrowserType { get; set; }
