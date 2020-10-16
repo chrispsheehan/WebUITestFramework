54be27a3491c5b4568b70c4f8904e37fed4650f0
chrispsheehan <chris_sheehan@hotmail.co.uk>
2020-10-16 13:48:41 +0100

    settings shape class

---
+++ src/Framework.Settings/TestSettings.cs
@@ -0,0 +1,10 @@
+namespace Framework.Settings
+{
+    public class TestSettings
+    {
+        public string BaseUrl { get; set; }
+        public double WaitSeconds { get; set; }
+        public string BrowserName { get; set; }
+        public string BrowserType { get; set; }
+    }
+}
