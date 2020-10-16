c6db122c1c7e9fbe6c4dbe51531fb56df531a628
chrispsheehan <chris_sheehan@hotmail.co.uk>
2020-10-16 13:56:04 +0100

    Setting config added

---
+++ src/Framework.Settings/SettingsConfiguration.cs
@@ -0,0 +1,37 @@
+namespace Framework.Settings
+{
+    public static class SettingsConfiguration
+    {
+        public static TestSettings GetSettings(string settingsFileName)
+        {
+            var configRoot = BuildConfig(settingsFileName);
+
+            return new TestSettings
+            {
+                BaseUrl = configRoot.GetAppSetting("BaseUrl"),
+                WaitSeconds = configRoot.GetAppSetting("WaitSeconds"),
+                BrowserName = configRoot.GetAppSetting("BrowserName"),
+                BrowserType  = configRoot.GetAppSetting("BrowserType")
+            };  
+        }
+
+        private static IConfigurationRoot BuildConfig(string settingsFileName)
+        {
+            return new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile(settingsFileName)
+                .Build();
+        }
+
+        private static string GetAppSetting(this IConfigurationRoot config, string settingName)
+        {
+            string content = config[settingName];
+
+            if (!string.IsNullOrEmpty(content))
+            {
+                return content;
+            }
+            throw new InvalidOperationException($"Could not read app setting for {settingName}");
+        }        
+    }
+}
